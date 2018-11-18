aaf755dedd46f5c6f7709bb3e1a425680f4100a9
unknown <you@example.com>
2018-11-19 00:09:54 +0800

    qq

---
+++ src/main.js
@@ -0,0 +1,17 @@
+/*
+入口JS
+ */
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import TopHeader from './components/TopHeader/TopHeader.vue'
+
+
+Vue.component('TopHeader', TopHeader)
+
+
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
